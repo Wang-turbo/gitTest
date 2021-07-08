3a622494fa82a25d4d70da656db9e2e9ecac3986
wangjunying <wjy01082200@antgroup.com>
2021-07-08 11:06:33 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-// 获取数据类型
-(function (root, globalName, factory) {
-    if (typeof define === 'function' && define.amd) {
-      // AMD:
-      define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-      // Node:
-      module.exports = factory();
-      module.exports.default = module.exports;
-    } else {
-      // Browser:
-      window[globalName] = factory();
-    }
-  }(this, 'dataType', function () {
-    'use strict';
-  
-    return function dataType (data) {
-      return ({}).toString.call(data).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-    }
-  })
-);
+const getType = (data) =>{
+  var type = typeof data;
+  //如果不是object类型的数据，直接用typeof就能判断出来
+  if (type !== 'object') {
+      return type;
+  }
+  //如果是object类型数据，准确判断类型必须使用Object.prototype.toString.call(obj)的方式才能判断
+  return Object.prototype.toString.call(data).match(/\s([a-zA-Z]+)/)[1];
+  //.replace(/^\[object (\S+)\]$/, '$1');  主要是把  第一个对象去掉
+}
+export default getType
