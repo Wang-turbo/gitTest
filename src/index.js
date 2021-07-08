d4c147472e14c4d260fca4c2df9f32b96287654f
wangjunying <wjy01082200@antgroup.com>
2021-07-08 13:29:26 +0800

    aa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const getType = (data) =>{
+const dataType = (data) =>{
   var type = typeof data;
   //如果不是object类型的数据，直接用typeof就能判断出来
   if (type !== 'object') {
@@ -7,4 +7,4 @@ const getType = (data) =>{
   //如果是object类型数据，准确判断类型必须使用Object.prototype.toString.call(obj)的方式才能判断
   return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
 }
-export default getType
+// export default getType
