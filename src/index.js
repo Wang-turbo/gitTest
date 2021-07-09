b65b8d94f6849087fcaa0771bf2a4761cde56da6
wangjunying <wjy01082200@antgroup.com>
2021-07-09 15:47:58 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-// const dataType = {}
 const dataType = (data) =>{
   //如果不是object类型的数据，直接用typeof就能判断出来
-  if (typeof data !== 'object') {
+  if (typeof data === 'object') {
+    if (data === null) return 'null'
+    if (data instanceof Array)  return 'array'
+    if (data instanceof Date) return 'date'
+    if (data instanceof RegExp) return 'regexp'
+    return 'object'
+  } else {
     return typeof data // undefined number string boolean symbol function
   }
-  if (data === null) {
-      return 'null'
-  }
-  if (data instanceof Array) {
-      return 'array'
-  }
-  if (data instanceof Date) {
-      return 'date'
-  }
-  if (data instanceof RegExp) {
-      return 'regexp'
-  }
-  if (data instanceof Object) {
-      return 'object'
-  }
+  
 }
 module.exports = dataType;
